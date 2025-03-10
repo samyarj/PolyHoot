97f87049b1abadaaae691c47fdf7eec976337b2d
Amelie <113046455+amelie-bou@users.noreply.github.com>
2025-03-09 21:58:59 -0400

    feat: added QRL support for player

--- client_leger/lib/UI/play/pages/player_game_page.dart
+++ client_leger/lib/UI/play/pages/player_game_page.dart
@@ -18,9 +18,10 @@ class PlayerGamePage extends ConsumerStatefulWidget {
 }
 
 class _PlayerGamePageState extends ConsumerState<PlayerGamePage> {
-  int? _selectedChoiceIndex;
   final WebSocketManager _socketManager = WebSocketManager.instance;
   bool shouldDisconnect = true;
+  static const int MAX_CHARACTERS = 200;
+  final TextEditingController _QRLanswerController = TextEditingController();
 
   @override
   void dispose() {
@@ -28,6 +29,7 @@ class _PlayerGamePageState extends ConsumerState<PlayerGamePage> {
       AppLogger.i("Disconnecting player from game");
       _socketManager.webSocketSender(DisconnectEvents.Player.value);
     }
+    _QRLanswerController.dispose();
     super.dispose();
   }
 
@@ -53,91 +55,122 @@ class _PlayerGamePageState extends ConsumerState<PlayerGamePage> {
       }
     });
 
-    return Padding(
+    return SingleChildScrollView(
+      physics: NeverScrollableScrollPhysics(),
       padding: const EdgeInsets.all(16.0),
-      child: Column(
-        children: [
-          SizedBox(height: 16),
-          Text(playerGameState.quizTitle,
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-          SizedBox(height: 16),
-          Text("Vos points: ${playerGameState.playerPoints.toString()}",
-              style: TextStyle(color: colorScheme.onSurface, fontSize: 20)),
-          SizedBox(height: 16),
-          Row(
-            children: [
-              Text(
-                "${playerGameState.currentQuestion.points.toString()} points",
-                style: TextStyle(color: colorScheme.onSurface, fontSize: 20),
-              ),
-              SizedBox(width: 16),
-              if (playerGameState.playerInfo.waitingForQuestion)
+      child: SizedBox(
+        height: MediaQuery.of(context).size.height,
+        child: Column(
+          children: [
+            SizedBox(height: 16),
+            Text(playerGameState.quizTitle,
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+            SizedBox(height: 16),
+            Text("Vos points: ${playerGameState.playerPoints.toString()}",
+                style: TextStyle(color: colorScheme.onSurface, fontSize: 20)),
+            SizedBox(height: 16),
+            Row(
+              children: [
                 Text(
-                  "La prochaine question commence dans ${playerGameState.time} seconde${playerGameState.time == 1 ? '' : 's'}.",
+                  "${playerGameState.currentQuestion.points.toString()} points",
                   style: TextStyle(color: colorScheme.onSurface, fontSize: 20),
                 ),
-              if (!playerGameState.playerInfo.waitingForQuestion)
-                Expanded(
-                  child: Text(playerGameState.currentQuestion.text,
-                      style: TextStyle(fontSize: 20)),
-                ),
-              Column(
-                children: [
-                  if (!playerGameState.playerInfo.waitingForQuestion)
-                    Text(
-                      playerGameState.time.toString(),
-                      style: TextStyle(fontSize: 20),
-                    ),
-                  if (playerGameState.gamePaused)
-                    Text(
-                      "Jeu en pause",
-                      style: TextStyle(fontSize: 20),
-                    ),
-                ],
-              )
-            ],
-          ),
-          SizedBox(height: 16),
-          if (playerGameState.currentQuestion.type == QuestionType.QCM.name)
-            ...playerGameState.currentQuestion.choices!
-                .asMap()
-                .entries
-                .map((entry) {
-              int choiceIndex = entry.key;
-              final choice = entry.value;
-              return ListTile(
-                title: Text(choice.text, style: TextStyle(fontSize: 20)),
-                leading: Checkbox(
-                  value: playerGameState.playerInfo.choiceSelected[choiceIndex],
-                  onChanged: (value) {
-                    setState(() {
-                      playerGameNotifier.selectChoice(choiceIndex);
-                    });
-                  },
-                ),
-              );
-            }),
-          buildFeedbackMessage(
-              playerGameState.choiceFeedback, playerGameState.currentQuestion),
-          ElevatedButton(
-            onPressed: playerGameState.finalAnswer
-                ? null
-                : () {
-                    playerGameNotifier.finalizeAnswer();
-                  },
-            child: Text("Soumettre", style: TextStyle(fontSize: 20)),
-          ),
-          Spacer(),
-          Align(
-            alignment: Alignment.bottomRight,
-            child: ElevatedButton(
-              onPressed: () {
-                abandonGame();
-              },
-              child: Text("Abandonner", style: TextStyle(fontSize: 20)),
+                SizedBox(width: 16),
+                if (playerGameState.playerInfo.waitingForQuestion)
+                  Text(
+                    "La prochaine question commence dans ${playerGameState.time} seconde${playerGameState.time == 1 ? '' : 's'}.",
+                    style:
+                        TextStyle(color: colorScheme.onSurface, fontSize: 20),
+                  ),
+                if (!playerGameState.playerInfo.waitingForQuestion)
+                  Expanded(
+                    child: Text(playerGameState.currentQuestion.text,
+                        style: TextStyle(fontSize: 20)),
+                  ),
+                Column(
+                  children: [
+                    if (!playerGameState.playerInfo.waitingForQuestion)
+                      Text(
+                        playerGameState.time.toString(),
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    if (playerGameState.gamePaused)
+                      Text(
+                        "Jeu en pause",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                  ],
+                )
+              ],
             ),
-          ),
-        ],
+            SizedBox(height: 16),
+            if (playerGameState.currentQuestion.type == QuestionType.QCM.name)
+              ...playerGameState.currentQuestion.choices!
+                  .asMap()
+                  .entries
+                  .map((entry) {
+                int choiceIndex = entry.key;
+                final choice = entry.value;
+                return ListTile(
+                  title: Text(choice.text, style: TextStyle(fontSize: 20)),
+                  leading: Checkbox(
+                    value:
+                        playerGameState.playerInfo.choiceSelected[choiceIndex],
+                    onChanged: (value) {
+                      setState(() {
+                        playerGameNotifier.selectChoice(choiceIndex);
+                      });
+                    },
+                  ),
+                );
+              }),
+            if (playerGameState.currentQuestion.type == QuestionType.QRL.name &&
+                !playerGameState.playerInfo.submitted)
+              Text(
+                'Caractères restants : ${MAX_CHARACTERS - _QRLanswerController.text.length}',
+                style: TextStyle(color: colorScheme.onSurface, fontSize: 16),
+              ),
+            if (playerGameState.currentQuestion.type == 'QRL')
+              TextField(
+                controller: _QRLanswerController,
+                maxLength: MAX_CHARACTERS,
+                enabled: !playerGameState.playerInfo.submitted,
+                decoration: InputDecoration(
+                  hintText: 'Votre réponse',
+                  border: OutlineInputBorder(),
+                ),
+                onChanged: (value) {
+                  setState(() {});
+                },
+              ),
+            buildFeedbackMessage(playerGameState.choiceFeedback,
+                playerGameState.currentQuestion),
+            ElevatedButton(
+              onPressed: playerGameState.playerInfo.submitted
+                  ? null
+                  : () {
+                      playerGameNotifier.finalizeAnswer();
+                      if (playerGameState.currentQuestion.type ==
+                          QuestionType.QRL.name) {
+                        playerGameNotifier
+                            .sendAnswerForCorrection(_QRLanswerController.text);
+                        _QRLanswerController.clear();
+                      }
+                    },
+              child: Text("Soumettre", style: TextStyle(fontSize: 20)),
+            ),
+            Spacer(),
+            Align(
+              alignment: Alignment.bottomRight,
+              child: ElevatedButton(
+                onPressed: () {
+                  abandonGame();
+                },
+                child: Text("Abandonner", style: TextStyle(fontSize: 20)),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
