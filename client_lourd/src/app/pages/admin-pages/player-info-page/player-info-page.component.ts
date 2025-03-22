bf676465ceac309add26e2e12bbc3dbcd62d2586
Samyar Janatian <samyar.mahmoudi@polymtl.ca>
2025-03-22 16:19:24 -0400

    style(admin): add table for player info

--- client_lourd/src/app/pages/admin-pages/player-info-page/player-info-page.component.ts
+++ client_lourd/src/app/pages/admin-pages/player-info-page/player-info-page.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-player-info-page',
-  templateUrl: './player-info-page.component.html',
-  styleUrls: ['./player-info-page.component.scss']
+    selector: 'app-player-info-page',
+    templateUrl: './player-info-page.component.html',
+    styleUrls: ['./player-info-page.component.scss'],
 })
-export class PlayerInfoPageComponent {
-
-}
+export class PlayerInfoPageComponent {}
