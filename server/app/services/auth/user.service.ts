59540b709f78fa7b3f657a7311e25c287d18784b
Minutu <113061404+Minutu@users.noreply.github.com>
2025-03-23 22:44:15 -0500

    fix: prevent double connection logging.

--- server/app/services/auth/user.service.ts
+++ server/app/services/auth/user.service.ts
@@ -15,9 +15,11 @@ export class UserService {
     constructor(private readonly cloudinaryService: CloudinaryService) {}
 
     addUserToMap(socketId: string, uid: string) {
-        this.usersSocketIdMap.set(socketId, uid);
-        this.setLog(uid, 'connect').catch((error) => console.error('Failed to log connection:', error));
-        this.logger.log(`User ${uid} connected with socket ID ${socketId}`);
+        if (!this.isUserInMap(socketId)) {
+            this.usersSocketIdMap.set(socketId, uid);
+            this.setLog(uid, 'connect').catch((error) => console.error('Failed to log connection:', error));
+            this.logger.log(`User ${uid} connected with socket ID ${socketId}`);
+        }
     }
 
     isUserInMap(socketId: string): boolean {
