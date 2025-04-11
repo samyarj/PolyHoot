71bd5c39b139a08809101a48ce35b2a695db8bfd
Amelie <113046455+amelie-bou@users.noreply.github.com>
2025-04-11 12:07:00 -0400

    feat: added push notif for ingame chat

--- server/app/gateways/chat/chat.gateway.ts
+++ server/app/gateways/chat/chat.gateway.ts
@@ -1,21 +1,27 @@
 import { ChatEvents } from '@app/constants/enum-classes';
+import { UserService } from '@app/services/auth/user.service';
 import { ChatService } from '@app/services/chat/chat.service';
 import { GameManagerService } from '@app/services/game-manager/game-manager.service';
+import { PushNotifService } from '@app/services/push-notif/push-notif.service';
 import { QuickReplyService } from '@app/services/quick-reply/quick-reply.service';
 import { ChatMessage } from '@common/chat-message';
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 @WebSocketGateway({ cors: true })
 @Injectable()
 export class ChatGateway {
     @WebSocketServer() private server: Server;
+    private readonly logger = new Logger(ChatGateway.name);
+
 
     constructor(
         private chatService: ChatService,
         private quickReplyService: QuickReplyService,
         private gameManagerService: GameManagerService,
-    ) {}
+        private userService: UserService,
+        private pushNotifService: PushNotifService,
+    ) { }
 
     @SubscribeMessage(ChatEvents.RoomMessage)
     async roomMessage(clientSocket: Socket, message: ChatMessage) {
@@ -23,6 +29,27 @@ export class ChatGateway {
         const clientRoomId: string = Array.from(clientSocket.rooms.values())[1];
         this.chatService.addMessage(message, clientRoomId);
         this.server.to(clientRoomId).emit(ChatEvents.MessageAdded, message);
+
+        // push notif 
+        try {
+            const game = this.gameManagerService.getGameByRoomId(clientRoomId);
+            game.players.forEach((player) => {
+                if (player.isInGame) {
+                    const userUid = this.userService.getUserUidFromMap(player.socket.id);
+                    if (userUid && userUid != message.uid) {
+                        this.pushNotifService.onIngameMessage(message.message, userUid);
+                    }
+                }
+            });
+            if (game.organizer.isInGame) {
+                const organizerUid = this.userService.getUserUidFromMap(game.organizer.socket.id);
+                if (organizerUid && organizerUid != message.uid) {
+                    this.pushNotifService.onIngameMessage(message.message, organizerUid);
+                }
+            }
+        } catch (e) {
+            this.logger.debug("Couldnt find game or players to send push notif");
+        }
     }
 
     @SubscribeMessage(ChatEvents.GetHistory)
